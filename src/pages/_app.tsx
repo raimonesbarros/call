2374674a2284a3eac378e30f63302305c3cd73f8
Raimones Barros <raimonesrg3@gmail.com>
2023-07-14 17:27:32 -0300

    feat: use extenal fonts and design system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
+
+globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
