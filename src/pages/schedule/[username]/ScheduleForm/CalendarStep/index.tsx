afc821c39add27c76f4388ba1390003833c6c678
Raimones Barros <raimonesrg3@gmail.com>
2023-08-01 20:44:29 -0300

    feat: create dinamic calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,4 +1,6 @@
 import { Calendar } from "../../../../../components/Calendar"
+import { useState } from "react"
+import dayjs from "dayjs"
 import {
   Container,
   TimePicker,
@@ -8,15 +10,22 @@ import {
 } from "./styles"
 
 export function CalendarStep() {
-  const isDateSelected = true
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format("DD[ de ]MMMM")
+    : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20 de setembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
